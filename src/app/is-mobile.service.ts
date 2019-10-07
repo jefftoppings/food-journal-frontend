5c711dd516ef01af19f367ed2ce31e580752a630
Jeff Toppings <jeffrey.toppings@outlook.com>
2019-10-06 20:08:57 -0600

    create isMobile service

---
+++ src/app/is-mobile.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import {map, shareReplay} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IsMobileService {
+
+  isMobile$: Observable<boolean>;
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.isMobile$ = this.breakpointObserver.observe(Breakpoints.Handset)
+      .pipe(
+        map(result => result.matches),
+        shareReplay()
+      );
+  }
+}
